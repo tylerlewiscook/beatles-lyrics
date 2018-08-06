e4d84a0377a4f004246c7950fccf643e1fe23cce
tylerlewiscook <8811690+tylerlewiscook@users.noreply.github.com>
2018-08-05 19:46:41 -0500

    Add files via upload

---
+++ sgt_peppers_wordcloud.R
@@ -0,0 +1,27 @@
+library(tm)
+library(wordcloud)
+
+setwd("Beatles/")
+
+
+
+# Create document term matrix after some processing -------------------------
+
+sgtp <- VCorpus(DirSource("SgtPeppers/"))
+
+sgtp <- tm_map(sgtp, removePunctuation)
+sgtp <- tm_map(sgtp, content_transformer(tolower))
+sgtp <- tm_map(sgtp, removeNumbers)
+sgtp <- tm_map(sgtp, stripWhitespace)
+sgtp <- tm_map(sgtp, removeWords, stopwords("english")) 
+
+dtm <- DocumentTermMatrix(sgtp)
+
+freq <- colSums(as.matrix(dtm))
+
+
+
+# Word cloud -----------------------------------------------------------------
+
+set.seed(1)
+wordcloud(names(freq), freq, min.freq = 5, colors = brewer.pal(6, "Blues"))
